85d87893595a6c6e6fd882aee455a46bd3eb2b83
wlkeith <39680357+wlkeith@users.noreply.github.com>
2019-12-16 11:37:50 -0600

    Uploaded ruby notes file
    Contains notes & info on ruby w/ code snippets

---
+++ RubyonRailsCourse.rb
@@ -0,0 +1,33 @@
+
+
+#Ruby basics:
+#Arrays
+
+
+#Array in ruby:
+address = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#Built in array reverse method:
+address.reverse!
+
+
+#use repl to test this reverse method: repl.it
+#stands for read, evaluate, print, loop
+#provides dev environment for lots of different languages
+
+#print arrray
+p address
+
+
+#print array at address location
+p address[5]
+
+
+#reverse the array
+new_address = address.reverse!
+
+
+#print the new array
+p new_address
+
+
